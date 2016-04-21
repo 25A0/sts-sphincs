bfb034fe8732cded08cc373641fd4dc20d39f426
Moritz Neikes <mneikes@gmx.de>
2016-04-21 05:56:41 +0200

    Add blake(256|512) reference implementations

---
+++ sphincs256/ref/crypto_hash_blake256.h
@@ -0,0 +1,1 @@
+void crypto_hash_blake256(unsigned char *out, unsigned char *in, unsigned long long inlen);
---
+++ sphincs256/ref/crypto_hash_blake512.h
@@ -0,0 +1,1 @@
+void crypto_hash_blake512(unsigned char *out, unsigned char *in, unsigned long long inlen);
