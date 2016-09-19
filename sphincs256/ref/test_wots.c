a98fb529fb62ca98879bbb2a9caa9962c369587a
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2016-09-19 23:23:52 +0200

    Update WOTS tests

--- sphincs256/ref/test_wots.c
+++ sphincs256/ref/test_wots.c
@@ -18,16 +18,24 @@ int test01()
   // Generate keypair
   unsigned char pk[WOTS_L*HASH_BYTES];
   unsigned char sk[SEED_BYTES];
-  unsigned char masks[(WOTS_W-1)*HASH_BYTES];
-  randombytes(masks, (WOTS_W-1)*HASH_BYTES);
-  wots_pkgen(pk, sk, masks);
+  unsigned char seed[PUBLIC_SEED_BYTES];
+  randombytes(seed, PUBLIC_SEED_BYTES);
+  randombytes(sk, SEED_BYTES);
+
+  uint32_t addr[ADDR_SIZE];
+  int i;
+  for(i = 0; i < ADDR_SIZE; i++) {
+    addr[i] = 0;
+  }
+
+  wots_pkgen(pk, sk, seed, addr);
 
   // Message
   // wots will always sign hashes, so we use a message
   // of length HASH_BYTES here.
   unsigned long long mlen = HASH_BYTES;
   unsigned char message[mlen];
-  unsigned int i = 0;
+
   message[i++] = 'H';
   message[i++] = 'e';
   message[i++] = 'l';
@@ -45,24 +53,68 @@ int test01()
   // Create signature
   unsigned char sig[WOTS_L*HASH_BYTES];
 
-  wots_sign(sig, message, sk, masks);
+  wots_sign(sig, message, sk, seed, addr);
 
   // Verify signature
 
   // Generated pk:
   unsigned char gpk[WOTS_L*HASH_BYTES];
 
-  wots_verify(gpk, sig, message, masks);
+  wots_verify(gpk, sig, message, seed, addr);
 
   return compare(pk, gpk, WOTS_L*HASH_BYTES);
+}
+
+int test02()
+{
+  unsigned char sk[HASH_BYTES];
+  randombytes(sk, HASH_BYTES);
+
+  unsigned char seed[PUBLIC_SEED_BYTES];
+  randombytes(seed, PUBLIC_SEED_BYTES);
+
+  uint32_t addr[ADDR_SIZE];
+  int i;
+  for(i = 0; i < ADDR_SIZE; i++) {
+    addr[i] = 0;
+  }
+  set_type(addr, WOTS_ADDR);
+  set_wots_ots_index(addr, 0);
+
+  unsigned char out[HASH_BYTES];
+  int err = 0;
+  gen_chain(out,
+            sk,
+            seed,
+            addr,
+            WOTS_W - 1,
+            0);
+  for(i = 0; i < WOTS_W - 1; i++) {
+    unsigned char buf[HASH_BYTES];
+    gen_chain(buf, sk, seed, addr, i, 0);
+    gen_chain(buf, buf, seed, addr, WOTS_W - 1 - i, i);
+    err |= compare(out, buf, HASH_BYTES);
+  }
+
+  return err;
+}
+
+int run_test(int (*test_fun)(void), char* description)
+{
+  int err = (*test_fun)();
+  if(err) {
+    printf("Fail in: %s\n", description);
+  }
+  return err;
 }
 
 int main(int argc, char const *argv[])
 {
   int err = 0;
-  
-  err |= test01();
-  
+
+  err |= run_test(&test01, "Test WOTS as a whole");
+  err |= run_test(&test02, "Test chaining function");
+
   if(err)
   {
     printf("Expected and actual results differed.\n");
