a9a0a89f35f8be39587b0b8f830dd5c16cda4062
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2016-10-04 10:18:49 +0200

    Switch tree.c to multi-target-resistant hashing

--- sphincs256/ref/tree.c
+++ sphincs256/ref/tree.c
@@ -6,7 +6,7 @@
 
 void get_seed(unsigned char seed[SEED_BYTES],
               const unsigned char *sk,
-              const leafaddr *a)
+              uint32_t *address)
 {
 #if (N_LEVELS > 15) && (N_LEVELS < 8)
 #error "Need to have 8 <= N_LEVELS <= 15"
@@ -19,50 +19,59 @@ void get_seed(unsigned char seed[SEED_BYTES],
 #if TOTALTREE_HEIGHT != 60
 #error "Need to have TOTALTREE_HEIGHT == 60"
 #endif
-  unsigned char buffer[SEED_BYTES+8];
-  unsigned long long t;
+
+  set_type(address, WOTS_ADDR);
+
+  // TODO (moritz): # of used bytes is currently hard-coded to 16 so that only
+  // the SPHINCS part of the hash_address is used. I should check if instead we
+  // can use the entire address, or at least use a better method to determine
+  // how much of the address should be used.
+  unsigned char buffer[SEED_BYTES+16];
   int i;
 
   for(i=0;i<SEED_BYTES;i++)
     buffer[i] = sk[i];
 
-  //4 bits to encode level
-  t  = a->level;
-  //55 bits to encode subtree
-  t |= a->subtree << 4;
-  //5 bits to encode leaf
-  t |= (unsigned long long)a->subleaf << 59;
+  for(i = 0; i < 16; i++) {
+    buffer[SEED_BYTES + i] = ((unsigned char*) address)[i];
+  }
 
-  for(i=0;i<8;i++)
-    buffer[SEED_BYTES+i] = (t >> 8*i) & 0xff;
-  
 #if SEED_BYTES != HASH_BYTES
 #error "Need to have SEED_BYTES == HASH_BYTES"
 #endif
-  varlen_hash(seed,buffer,SEED_BYTES+8);
+  varlen_hash(seed,buffer,SEED_BYTES+16);
 }
 
 
 // Hashes two nodes in a binary tree to form its parent node.
 // HASH_BYTES bytes will be written to *parent, and
 // 2*HASH_BYTES bytes will be read from *nodes.
+// *address should point to the address of the parent node
+// that is being constructed
 static void hash_nodes(unsigned char *parent,
                        const unsigned char *nodes,
-                       const unsigned char *masks)
+                       const uint32_t *address,
+                       const unsigned char *public_seed)
 {
-  hash_2n_n_mask(parent, nodes, masks);
+  hash_2n_n_addr_seeded(parent, nodes, (unsigned char*) address, public_seed);
 }
 
+// *address should point to the l_tree that is being constructed.
+// the type of the address will be changed to WOTS_L_ADDR.
 void l_tree(unsigned char *leaf,
             unsigned char *wots_pk,
-            const unsigned char *masks)
+            uint32_t *address,
+            const unsigned char *public_seed)
 {
+  set_type(address, WOTS_L_ADDR);
   int l = WOTS_L;
   int i,j = 0;
   for(i=0;i<WOTS_LOG_L;i++)
   {
-    for(j=0 ;j < (l>>1);j++)
-      hash_nodes(wots_pk+j*HASH_BYTES,wots_pk+j*2*HASH_BYTES, masks+i*2*HASH_BYTES);
+    for(j=0 ;j < (l>>1);j++) {
+      set_wots_l_node(address, node_index(WOTS_LOG_L, i+1, j << 1));
+      hash_nodes(wots_pk+j*HASH_BYTES,wots_pk+j*2*HASH_BYTES, address, public_seed);
+    }
 
     if(l&1)
     {
@@ -77,56 +86,60 @@ void l_tree(unsigned char *leaf,
 
 
 void gen_leaf_wots(unsigned char leaf[HASH_BYTES],
-                   const unsigned char *masks,
                    const unsigned char *sk,
-                   const leafaddr *a,
+                   uint32_t *addr,
                    const unsigned char *public_seed)
 {
   unsigned char seed[SEED_BYTES];
   unsigned char pk[WOTS_L*HASH_BYTES];
 
-  int i;
-
-  uint32_t addr[ADDR_SIZE];
-  for(i = 0; i < ADDR_SIZE; i++) {
-    addr[i] = 0;
-  }
-
-  get_seed(seed, sk, a);
+  get_seed(seed, sk, addr);
   wots_pkgen(pk, seed, public_seed, addr);
 
-  l_tree(leaf, pk, masks);
+  l_tree(leaf, pk, addr, public_seed);
 }
 
 
 void treehash(unsigned char *node,
               int height,
               const unsigned char *sk,
-              const leafaddr *leaf,
-              const unsigned char *masks,
+              uint32_t *address,
               const unsigned char *public_seed)
 {
 
-  leafaddr a = *leaf;
-  int lastnode,i;
+  int i, layer;
   unsigned char stack[(height+1)*HASH_BYTES];
   unsigned int  stacklevels[height+1];
   unsigned int  stackoffset=0;
-  unsigned int maskoffset =0;
 
-  lastnode = a.subleaf+(1<<height);
+  uint32_t subtree_node = get_sphincs_subtree_node(address);
+  uint32_t lastnode = subtree_node + (1<<height);
 
-  for(;a.subleaf<lastnode;a.subleaf++) 
+  for( ; subtree_node < lastnode; subtree_node++)
   {
-    gen_leaf_wots(stack+stackoffset*HASH_BYTES,masks,sk,&a,public_seed);
+    set_sphincs_subtree_node(address, subtree_node);
+    gen_leaf_wots(stack+stackoffset*HASH_BYTES, sk, address, public_seed);
+
     stacklevels[stackoffset] = 0;
     stackoffset++;
+
     while(stackoffset>1 && stacklevels[stackoffset-1] == stacklevels[stackoffset-2])
     {
-      //MASKS
-      maskoffset = 2*(stacklevels[stackoffset-1] + WOTS_LOG_L)*HASH_BYTES;
-      hash_nodes(stack+(stackoffset-2)*HASH_BYTES,stack+(stackoffset-2)*HASH_BYTES,
-          masks+maskoffset);
+
+      // the layer of the node that we calculate:
+      layer = stacklevels[stackoffset-1]+1;
+      // the index of the node in that layer is the current subtree_node,
+      // shifted to the right by the height of the current layer.
+      set_sphincs_subtree_node(address,
+                               node_index(height,
+                                          layer,
+                                          subtree_node >> layer));
+      set_type(address, SPHINCS_ADDR);
+
+      hash_nodes(stack+(stackoffset-2)*HASH_BYTES,
+                 stack+(stackoffset-2)*HASH_BYTES,
+                 address,
+                 public_seed);
       stacklevels[stackoffset-2]++;
       stackoffset--;
     }
@@ -136,15 +149,22 @@ void treehash(unsigned char *node,
 }
 
 
+// address should point to the leaf in the SPHINCS subtree
+// that is authenticated by the auth path
 void validate_authpath(unsigned char root[HASH_BYTES],
                        const unsigned char leaf[HASH_BYTES],
-                       unsigned int leafidx,
+                       uint32_t *address,
+                       const unsigned char *public_seed,
                        const unsigned char *authpath,
-                       const unsigned char *masks,
                        unsigned int height)
 {
   int i,j;
   unsigned char buffer[2*HASH_BYTES];
+  unsigned int leafidx = get_sphincs_subtree_node(address);
+  // Copy of the leaf index so that we can restore it later
+  unsigned int idx = leafidx;
+
+  set_type(address, SPHINCS_ADDR);
 
   if(leafidx&1)
   {
@@ -167,71 +187,86 @@ void validate_authpath(unsigned char root[HASH_BYTES],
     leafidx >>= 1;
     if(leafidx&1)
     {
-      hash_nodes(buffer+HASH_BYTES,buffer,masks+2*(WOTS_LOG_L+i)*HASH_BYTES);
+      set_sphincs_subtree_node(address, node_index(height, i+1, leafidx));
+      hash_nodes(buffer+HASH_BYTES,buffer,address, public_seed);
+
       for(j=0;j<HASH_BYTES;j++)
         buffer[j] = authpath[j];
     }
     else
     {
-      hash_nodes(buffer,buffer,masks+2*(WOTS_LOG_L+i)*HASH_BYTES);
+      set_sphincs_subtree_node(address, node_index(height, i+1, leafidx));
+      hash_nodes(buffer,buffer,address, public_seed);
+
       for(j=0;j<HASH_BYTES;j++)
         buffer[j+HASH_BYTES] = authpath[j];
     }
     authpath += HASH_BYTES;
   }
-  hash_nodes(root,buffer,masks+2*(WOTS_LOG_L+height-1)*HASH_BYTES);
+  set_sphincs_subtree_node(address, node_index(height, height, 0));
+  hash_nodes(root,buffer,address, public_seed);
+
+  // reset leafnode index in address
+  set_sphincs_subtree_node(address, idx);
 }
 
 void compute_authpath_wots(unsigned char root[HASH_BYTES],
                            unsigned char *authpath,
-                           const leafaddr *a,
+                           uint32_t *address,
                            const unsigned char *sk,
-                           const unsigned char *masks,
                            unsigned int height,
                            const unsigned char *public_seed)
 {
   int i, idx, j;
-  leafaddr ta = *a;
+  // The index of the node that will be authenticated with the auth path
+  int node = get_sphincs_subtree_node(address);
 
   unsigned char tree[2*(1<<SUBTREE_HEIGHT)*HASH_BYTES];
   unsigned char seed[(1<<SUBTREE_HEIGHT)*SEED_BYTES];
   unsigned char pk[(1<<SUBTREE_HEIGHT)*WOTS_L*HASH_BYTES];
 
-  uint32_t addr[ADDR_SIZE];
-  for(i = 0; i < ADDR_SIZE; i++) {
-    addr[i] = 0;
+  // level 0
+  for(i = 0; i < (1<<SUBTREE_HEIGHT); i++) {
+    set_sphincs_subtree_node(address, i);
+    get_seed(seed + i * SEED_BYTES, sk, address);
   }
 
-
-  // level 0
-  for(ta.subleaf = 0; ta.subleaf < (1<<SUBTREE_HEIGHT); ta.subleaf++)
-    get_seed(seed + ta.subleaf * SEED_BYTES, sk, &ta);
-
-  for(ta.subleaf = 0; ta.subleaf < (1<<SUBTREE_HEIGHT); ta.subleaf++)
-    wots_pkgen(pk + ta.subleaf * WOTS_L*HASH_BYTES,
-               seed + ta.subleaf * SEED_BYTES,
+  for(i = 0; i < (1<<SUBTREE_HEIGHT); i++) {
+    set_sphincs_subtree_node(address, i);
+    wots_pkgen(pk + i * WOTS_L*HASH_BYTES,
+               seed + i * SEED_BYTES,
                public_seed,
-               addr);
+               address);
+  }
 
-  for(ta.subleaf = 0; ta.subleaf < (1<<SUBTREE_HEIGHT); ta.subleaf++)
-    l_tree(tree + (1<<SUBTREE_HEIGHT)*HASH_BYTES + ta.subleaf * HASH_BYTES,
-        pk  + ta.subleaf * WOTS_L*HASH_BYTES, masks);
+  for(i = 0; i < (1<<SUBTREE_HEIGHT); i++) {
+    set_sphincs_subtree_node(address, i);
+    l_tree(tree + (1<<SUBTREE_HEIGHT)*HASH_BYTES + i * HASH_BYTES,
+           pk  + i * WOTS_L*HASH_BYTES,
+           address,
+           public_seed);
+  }
 
   int level = 0;
+
+  set_type(address, SPHINCS_ADDR);
 
   // tree
   for (i = (1<<SUBTREE_HEIGHT); i > 0; i>>=1)
   {
-    for (j = 0; j < i; j+=2)
-      hash_nodes(tree + (i>>1)*HASH_BYTES + (j>>1) * HASH_BYTES, 
-          tree + i*HASH_BYTES + j * HASH_BYTES,
-          masks+2*(WOTS_LOG_L + level)*HASH_BYTES);
+    for (j = 0; j < i; j+=2) {
+      set_sphincs_subtree_node(address, node_index(height, level+1, j >> 1));
+      hash_nodes(tree + (i>>1)*HASH_BYTES + (j>>1) * HASH_BYTES,
+                 tree + i*HASH_BYTES + j * HASH_BYTES,
+                 address,
+                 public_seed);
+    }
 
     level++;
   }
 
 
-  idx = a->subleaf;
+  idx = node;
 
   // copy authpath
   for(i=0;i<height;i++)
@@ -239,4 +274,7 @@ void compute_authpath_wots(unsigned char root[HASH_BYTES],
 
   // copy root
   memcpy(root, tree+HASH_BYTES, HASH_BYTES);
+
+  // reset sphincs node address
+  set_sphincs_subtree_node(address, idx);
 }
--- sphincs256/ref/tree.h
+++ sphincs256/ref/tree.h
@@ -5,45 +5,38 @@
 #include "hash_address.h"
 
 
-typedef struct{
-  int level;
-  unsigned long long subtree;
-  int subleaf;
-} leafaddr;
-
 void get_seed(unsigned char seed[SEED_BYTES],
               const unsigned char *sk,
-              const leafaddr *a);
+              const uint32_t *address);
 
 void l_tree(unsigned char *leaf,
             unsigned char *wots_pk,
-            const unsigned char *masks);
+            uint32_t *address,
+            const unsigned char *public_seed);
 
 void gen_leaf_wots(unsigned char leaf[HASH_BYTES],
-                   const unsigned char *masks,
                    const unsigned char *sk,
-                   const leafaddr *a,
+                   uint32_t *addr,
                    const unsigned char *public_seed);
 
 void treehash(unsigned char *node,
               int height,
               const unsigned char *sk,
-              const leafaddr *leaf,
-              const unsigned char *masks,
+              uint32_t *address,
               const unsigned char *public_seed);
 
 void validate_authpath(unsigned char root[HASH_BYTES],
                        const unsigned char leaf[HASH_BYTES],
-                       unsigned int leafidx,
+                       uint32_t *address,
+                       const unsigned char *public_seed,
                        const unsigned char *authpath,
-                       const unsigned char *masks,
                        unsigned int height);
 
 void compute_authpath_wots(unsigned char root[HASH_BYTES],
                            unsigned char *authpath,
-                           const leafaddr *a,
+                           uint32_t *address,
                            const unsigned char *sk,
-                           const unsigned char *masks,
                            unsigned int height,
                            const unsigned char *public_seed);
+
 #endif
