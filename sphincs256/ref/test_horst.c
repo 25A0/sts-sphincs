7e9161f2e1ec578e97625fcb7d60a5bd6f1a6c94
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2016-10-10 22:02:58 +0200

    Add test for HORST with randomized address

--- sphincs256/ref/test_horst.c
+++ sphincs256/ref/test_horst.c
@@ -70,11 +70,76 @@ int test01() {
   else return compare(pk, gpk, HASH_BYTES);
 }
 
+int test02() {
+  unsigned long long mlen = 32;
+  unsigned char message[mlen];
+  unsigned int i = 0;
+  message[i++] = 'H';
+  message[i++] = 'e';
+  message[i++] = 'l';
+  message[i++] = 'l';
+  message[i++] = 'o';
+  message[i++] = ' ';
+  message[i++] = 'W';
+  message[i++] = 'o';
+  message[i++] = 'r';
+  message[i++] = 'l';
+  message[i++] = 'd';
+  message[i++] = '!';
+  // Fill remainder with 0s
+  for (; i < mlen; ++i) { message[i] = 0; }
+  
+  unsigned char sig[HORST_SIGBYTES];
+  unsigned char pk[HASH_BYTES];
+  unsigned long long sigbytes;
+
+  // Initialize a not-so-random seed
+  unsigned char seed[SEED_BYTES];
+  for (i = 0; i < SEED_BYTES; ++i) { seed[i] = 0; }
+  prg(seed, SEED_BYTES, seed);
+
+  // construct address
+  uint32_t addr[ADDR_SIZE];
+
+  // Choose a random address
+  randombytes((unsigned char*) addr, ADDR_BYTES);
+
+  // Hash message
+  unsigned char m_hash[MSGHASH_BYTES];
+  msg_hash(m_hash, message, mlen);
+  
+  horst_sign(
+    sig,
+    pk,
+    &sigbytes, 
+    message, mlen, 
+    seed, 
+    addr,
+    m_hash
+  );
+
+  unsigned char gpk[HASH_BYTES];
+  int res = horst_verify(
+    gpk,
+    sig,
+    message,
+    mlen,
+    addr,
+    m_hash
+  );
+
+  if (res != 0) {
+    return res;
+  }
+  else return compare(pk, gpk, HASH_BYTES);
+}
+
 int main(int argc, char const *argv[])
 {
   int err = 0;
   
   err |= test01();
+  err |= test02();
   
   if(err)
   {
