4b92b2c081eab7c96dee710169d2caf235c44c6a
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2017-06-19 13:15:39 +0200

    Add API calls to WOTS that allows caller to use specific configuration

--- sphincs256/ref/wots.c
+++ sphincs256/ref/wots.c
@@ -3,14 +3,20 @@
 #include "hash.h"
 #include "hash_address.h"
 #include "assert.h"
+#include "wots.h"
+
+// A struct containing the default configuration
+const struct wots_config default_wots_config = {WOTS_L, WOTS_L1, WOTS_LOG_L,
+                                                WOTS_SIGBYTES};
 
 static void expand_seed(unsigned char out[WOTS_L*HASH_BYTES],
                         const unsigned char sk[SEED_BYTES],
-                        unsigned char addr[ADDR_BYTES])
+                        unsigned char addr[ADDR_BYTES],
+                        struct wots_config config)
 {
   int i;
   struct hash_addr address = init_hash_addr(addr);
-  for(i = 0; i < WOTS_L; i++) {
+  for(i = 0; i < config.wots_l; i++) {
     *address.wots_ots_index = i;
     *address.wots_ots_position = 0;
     hash_n_n_addr(out + i * HASH_BYTES, sk, addr);
@@ -66,11 +72,20 @@ void wots_pkgen(unsigned char pk[WOTS_L*HASH_BYTES],
                 const unsigned char seed[PUBLIC_SEED_BYTES],
                 unsigned char addr[ADDR_BYTES])
 {
+  wots_pkgen_conf(pk, sk, seed, addr, default_wots_config);
+}
+
+void wots_pkgen_conf(unsigned char pk[WOTS_L*HASH_BYTES],
+                const unsigned char sk[SEED_BYTES],
+                const unsigned char seed[PUBLIC_SEED_BYTES],
+                unsigned char addr[ADDR_BYTES],
+                struct wots_config config)
+{
   int i;
   set_type(addr, WOTS_ADDR);
-  expand_seed(pk, sk, addr);
+  expand_seed(pk, sk, addr, config);
   struct hash_addr address = init_hash_addr(addr);
-  for(i=0;i<WOTS_L;i++) {
+  for(i = 0; i < config.wots_l; i++) {
     *address.wots_ots_index = i;
     gen_chain(pk+i*HASH_BYTES, pk+i*HASH_BYTES, seed, addr, WOTS_W-1, 0);
   }
@@ -83,10 +98,20 @@ void wots_sign(unsigned char sig[WOTS_L*HASH_BYTES],
                const unsigned char seed[PUBLIC_SEED_BYTES],
                unsigned char addr[ADDR_BYTES])
 {
-  int basew[WOTS_L],i,c=0;
+  wots_sign_conf(sig, msg, sk, seed, addr, default_wots_config);
+}
+
+void wots_sign_conf(unsigned char sig[WOTS_L*HASH_BYTES],
+                    const unsigned char msg[HASH_BYTES],
+                    const unsigned char sk[SEED_BYTES],
+                    const unsigned char seed[PUBLIC_SEED_BYTES],
+                    unsigned char addr[ADDR_BYTES],
+                    struct wots_config config)
+{
+  int basew[config.wots_l],i,c=0;
 
 #if WOTS_W == 16
-  for(i=0;i<WOTS_L1;i+=2)
+  for(i=0;i<config.wots_l1;i+=2)
   {
     basew[i]   = msg[i/2] & 0xf;
     basew[i+1] = msg[i/2] >> 4;
@@ -94,7 +119,7 @@ void wots_sign(unsigned char sig[WOTS_L*HASH_BYTES],
     c += WOTS_W - 1 - basew[i+1];
   }
 
-  for( ;i<WOTS_L;i++)
+  for( ;i<config.wots_l;i++)
   {
     basew[i] = c & 0xf;
     c >>= 4;
@@ -102,14 +127,14 @@ void wots_sign(unsigned char sig[WOTS_L*HASH_BYTES],
 
   struct hash_addr address = init_hash_addr(addr);
   set_type(addr, WOTS_ADDR);
-  expand_seed(sig, sk, addr);
-  for(i=0;i<WOTS_L;i++) {
+  expand_seed(sig, sk, addr, config);
+  for(i=0;i<config.wots_l;i++) {
     *address.wots_ots_index = i;
     gen_chain(sig+i*HASH_BYTES, sig+i*HASH_BYTES, seed, addr, basew[i], 0);
   }
 
 #elif WOTS_W == 4
-  for(i=0;i<WOTS_L1;i+=4)
+  for(i=0;i<config.wots_l1;i+=4)
   {
     basew[i]   = msg[i/4] & 0x3;
     basew[i+1] = (msg[i/4] >> 2) & 0x3;
@@ -121,7 +146,7 @@ void wots_sign(unsigned char sig[WOTS_L*HASH_BYTES],
     c += WOTS_W - 1 - basew[i+3];
   }
 
-  for( ;i<WOTS_L;i++)
+  for( ;i<config.wots_l;i++)
   {
     basew[i] = c & 0x3;
     c >>= 2;
@@ -130,7 +155,7 @@ void wots_sign(unsigned char sig[WOTS_L*HASH_BYTES],
   struct hash_addr address = init_hash_addr(addr);
   set_type(addr, WOTS_ADDR);
   expand_seed(sig, sk, addr);
-  for(i=0;i<WOTS_L;i++) {
+  for(i=0;i<config.wots_l;i++) {
     *address.wots_ots_index = i;
     gen_chain(sig+i*HASH_BYTES, sig+i*HASH_BYTES, seed, addr, basew[i], 0);
   }
@@ -146,10 +171,20 @@ void wots_verify(unsigned char pk[WOTS_L*HASH_BYTES],
                  const unsigned char seed[PUBLIC_SEED_BYTES],
                  unsigned char addr[ADDR_BYTES])
 {
-  int basew[WOTS_L],i,c=0;
+  wots_verify_conf(pk, sig, msg, seed, addr, default_wots_config);
+}
+
+void wots_verify_conf(unsigned char pk[WOTS_L*HASH_BYTES],
+                      const unsigned char sig[WOTS_L*HASH_BYTES],
+                      const unsigned char msg[HASH_BYTES],
+                      const unsigned char seed[PUBLIC_SEED_BYTES],
+                      unsigned char addr[ADDR_BYTES],
+                      struct wots_config config)
+{
+  int basew[config.wots_l],i,c=0;
 
 #if WOTS_W == 16
-  for(i=0;i<WOTS_L1;i+=2)
+  for(i=0;i<config.wots_l1;i+=2)
   {
     basew[i]   = msg[i/2] & 0xf;
     basew[i+1] = msg[i/2] >> 4;
@@ -157,7 +192,7 @@ void wots_verify(unsigned char pk[WOTS_L*HASH_BYTES],
     c += WOTS_W - 1 - basew[i+1];
   }
 
-  for( ;i<WOTS_L;i++)
+  for( ;i<config.wots_l;i++)
   {
     basew[i] = c & 0xf;
     c >>= 4;
@@ -165,13 +200,13 @@ void wots_verify(unsigned char pk[WOTS_L*HASH_BYTES],
 
   struct hash_addr address = init_hash_addr(addr);
   set_type(addr, WOTS_ADDR);
-  for(i=0;i<WOTS_L;i++) {
+  for(i=0;i<config.wots_l;i++) {
     *address.wots_ots_index = i;
     gen_chain(pk+i*HASH_BYTES, sig+i*HASH_BYTES, seed, addr, WOTS_W-1-basew[i], basew[i]);
   }
 
 #elif WOTS_W == 4
-  for(i=0;i<WOTS_L1;i+=4)
+  for(i=0;i<config.wots_l1;i+=4)
   {
     basew[i]   = msg[i/4] & 0x3;
     basew[i+1] = (msg[i/4] >> 2) & 0x3;
@@ -183,7 +218,7 @@ void wots_verify(unsigned char pk[WOTS_L*HASH_BYTES],
     c += WOTS_W - 1 - basew[i+3];
   }
 
-  for( ;i<WOTS_L;i++)
+  for( ;i<config.wots_l;i++)
   {
     basew[i] = c & 0x3;
     c >>= 2;
@@ -191,7 +226,7 @@ void wots_verify(unsigned char pk[WOTS_L*HASH_BYTES],
 
   struct hash_addr address = init_hash_addr(addr);
   set_type(addr, WOTS_ADDR);
-  for(i=0;i<WOTS_L;i++) {
+  for(i=0;i<config.wots_l;i++) {
     *address.wots_ots_index = i;
     gen_chain(pk+i*HASH_BYTES, sig+i*HASH_BYTES, seed, addr, WOTS_W-1-basew[i], basew[i]);
   }
