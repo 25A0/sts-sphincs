d26d984bd19de4eeb153f2cb08281a28246f5fd2
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2020-01-16 08:23:09 +0100

    Better cycle printing for benchmarks

--- src/benchutils.c
+++ src/benchutils.c
@@ -12,7 +12,31 @@ int run_bench(int (*test_fun)(void), char* description)
 
 void print_cycles(const char* desc, unsigned long start, unsigned long end)
 {
-  printf("%24s: %24lu cycles\n", desc, end - start);
+  int cycle_width = 60;
+  unsigned char bar[cycle_width];
+  unsigned char clear[cycle_width];
+  int i = 0;
+  for(; i < cycle_width; i++) {
+    bar[i] = ((i+1) % 5) ? '-' : '+';
+    clear[i] = ' ';
+  }
+  bar[cycle_width - 1] = 0;
+  clear[cycle_width - 1] = 0;
+
+  int log_2 = 0;
+  unsigned long cycles = end - start;
+  while(cycles >>= 1) {
+    log_2++;
+  }
+
+  cycles = end - start;
+  double frac = (double) cycles / (double)((unsigned long)1 << log_2);
+
+  printf("%24s: %.*s%.*s %4.2f * 2^%2d cycles\n",
+         desc,
+         log_2, bar,
+         cycle_width - log_2, clear,
+         frac, log_2);
 }
 
 void print_bytes(const char* desc, unsigned long bytes)
